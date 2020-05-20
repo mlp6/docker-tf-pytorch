ec61bdb9d4474304c47faafc3f8f69e25050c72e
Mark Palmeri <mlp6@duke.edu>
2020-05-20 10:28:49 -0400

    make pytorch more functional
    * need to run in the container as root (for now) to get the Jupyter
      notebook to work
    * ignore notebook checkpoints

--- pytorch/launch.sh
+++ pytorch/launch.sh
@@ -1,2 +1,3 @@
 docker build -t pytorch .
-docker run --gpus all --privileged -it --rm -p 8888:8888 -p 6006:6006 -v $PWD:/tf -u `id -u $USER`:`id -g $GROUP` pytorch
+docker run --gpus all --privileged -it --rm -p 8888:8888 -p 6006:6006 -v $PWD:/tf pytorch
+#docker run --gpus all --privileged -it --rm -p 8888:8888 -p 6006:6006 -v $PWD:/tf -u `id -u $USER`:`id -g $GROUP` pytorch
---
+++ pytorch/launch_jupyter_lab.sh
@@ -0,0 +1,1 @@
+jupyter lab --no-browser --port=8888 --ip=0.0.0.0 --allow-root
